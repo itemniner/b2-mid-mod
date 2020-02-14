37753db6fb23144689462c162f7a4523adf98b34
Raymond Nguyen <rayknguyen@gmail.com>
2020-02-14 12:00:21 -0700

    Raymond Nguyen

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,9 @@
 class Actor < ApplicationRecord
   has_many :movie_actors
   has_many :movies, through: :movie_actors
+
+    def associated_actors(actor_id)
+    #if actor plays in the same movie as this actor print name
+    Movie.joins(:actor).where('actor_id = ?', actor_id)
+  end
 end
--- spec/features /actors/show_spec.rb
+++ spec/features /actors/show_spec.rb
@@ -13,11 +13,20 @@ RSpec.describe "On the index page", type: :feature do
       @star_wars_2 = Movie.create(name: "The Empire Strikes Back", studio: @studio_1)
       @star_wars_3 = Movie.create(name: "Return of the Jedi", studio: @studio_1)
 
-      @jackie = Actor.create(name: "Jackie Chan", age: 6)
-
       MovieActor.create(movie: @rush_hour, actor: @jackie)
       MovieActor.create(movie: @shanghai, actor: @jackie)
       MovieActor.create(movie: @medallion, actor: @jackie)
+
+      @jackie = Actor.create(name: "Jackie Chan", age: 65)
+      @chris = Actor.create(name: "Chris Tucker", age: 48)
+      @ken = Actor.create(name: "Ken Leung", age: 47)
+      @ray = Actor.create(name: "Ray Nguyen", age: 20)
+      @david = Actor.create(name: "David Holtcamp", age: 8)
+
+      MovieActor.create(movie: @rush_hour, actor: @jackie)
+      MovieActor.create(movie: @rush_hour, actor: @chris)
+      MovieActor.create(movie: @rush_hour, actor: @ken)
+      MovieActor.create(movie: @rush_hour, actor: @ray)
     end
     
     it "see the actor name, age and a list of the names of movies theyre in" do
@@ -31,7 +40,12 @@ RSpec.describe "On the index page", type: :feature do
     end
 
     it "see a unqiue list of all actors worked with" do
-      
+      visit "/actors/#{@jackie.id}"
+
+      expect(@jackie.associated_actors(@jackie.id)).to eq([@chris.name, @ken.name, @ray.name])
+      expect(page).to have_content(@chris.name)
+      expect(page).to have_content(@ken.name)
+      expect(page).to have_content(@ray.name)
     end
   end
 end
