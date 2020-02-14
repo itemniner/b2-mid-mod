ddc4d0d5c6467ea41316dec860dc46e476403b2a
Raymond Nguyen <rayknguyen@gmail.com>
2020-02-14 09:16:40 -0700

    Add test for story one

---
+++ spec/features /studios/index_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe "On the index page" do
+  it "see a list of all of the movie studios" do
+    studio = Studio.create(name: "WB")
+    studio_1 = Studio.create(name: "Lucus Films")
+   harry_potter = Movie.create(name: "Harry Potter", studio: studio)
+   departed = Movie.create(name: "Departed", studio: studio)
+   rush_hour = Movie.create(name: "Rush Hour", studio: studio)
+   star_wars_1 = Movie.create(name: "New Hope", studio: studio_1)
+   star_wars_2 = Movie.create(name: "The Empire Strikes Back", studio: studio_1)
+   star_wars_3 = Movie.create(name: "Return of the Jedi", studio: studio_1)
+
+   visit "/studios"
+
+    within("#studio-#{studio.id}") do
+      expect(page).to have_content(studio.name)
+      expect(page).to have_content(harry_potter.name)
+      expect(page).to have_content(departed.name)
+      expect(page).to have_content(rush_hour.name)
+    end 
+
+    within("#studio-#{studio_1.id}") do
+      expect(page).to have_content(studio_1.name)
+      expect(page).to have_content(star_wars_1)
+      expect(page).to have_content(star_wars_2)
+      expect(page).to have_content(star_wars_3)
+  end
+end
